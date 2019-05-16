1297bd488624f97bf3a424926303821872ad95a1
gauravdhavale <gauravdhavale@gmail.com>
2019-05-15 20:29:47 -0500

    Updates

---
+++ precache-manifest.89ef3195865f7a529a29fa66ec38172e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2d9e93ce5c1a6124b34a31ef7ea656",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f54bea164eee4be1306b",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3b407784061655e351b1",
+    "url": "/portfolio/static/css/main.c18a801e.chunk.css"
+  },
+  {
+    "revision": "f54bea164eee4be1306b",
+    "url": "/portfolio/static/js/2.4e2a0beb.chunk.js"
+  },
+  {
+    "revision": "3b407784061655e351b1",
+    "url": "/portfolio/static/js/main.5bbe0635.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "602444345dc7081511c7a10b6e02e0d4",
+    "url": "/portfolio/static/media/DataScience.60244434.png"
+  },
+  {
+    "revision": "6e12319f6a0e4243ccf95604ae11b9b3",
+    "url": "/portfolio/static/media/Profile1.6e12319f.png"
+  },
+  {
+    "revision": "e34e988a558baced9a8a7f5d8bd3d565",
+    "url": "/portfolio/static/media/background2.e34e988a.jpg"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "69f1e09004959cb840430ba466856eee",
+    "url": "/portfolio/static/media/machineLearning.69f1e090.png"
+  },
+  {
+    "revision": "639f0fa4b9b1554673f42c83a4b844b0",
+    "url": "/portfolio/static/media/otherProject.639f0fa4.jpg"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/portfolio/static/media/twitter_icon.512536cb.png"
+  }
+]);
